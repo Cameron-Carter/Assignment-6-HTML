19b5ffb57ccad572df407ca0118c00d5f11490b7
Ubuntu <ubuntu@ip-172-31-84-110.ec2.internal>
2020-05-09 15:11:30 +0000

    work

---
+++ myscript.js
@@ -0,0 +1,11 @@
+// JavaScript File
+let size;
+let toppings;
+
+function pizzaCost () {
+    size = document.getElementById("size").value
+    toppings = document.getElementById("toppings").value
+    if (toppings = l) {
+        
+    }
+}
